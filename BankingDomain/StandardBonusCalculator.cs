57bc1efa53c9711f660542d95575ba3997954f01
Jeffry Gonzalez <jeff@hypertheory.com>
2021-05-17 16:43:23 -0400

    done for the day

---
+++ BankingDomain/StandardBonusCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BankingDomain
+{
+    public class StandardBonusCalculator : ICalculateBonuses
+    {
+        private readonly ISystemTime _systemTime;
+
+        public StandardBonusCalculator(ISystemTime systemTime)
+        {
+            _systemTime = systemTime;
+        }
+
+        public decimal CalculateTheBonusFor(decimal amountToDeposit, decimal balance)
+        {
+            return _systemTime.GetCurrent().Hour >= 17 ? amountToDeposit * .05M : amountToDeposit * .07M;
+        }
+    }
+}
+
+/*
+ * "The best way to work with untestable code is to kick it into a corner until it learns to play nice
+ * - Jeremy D. Miller
+ * 
+ * 
+ * "Make all variables explicit."
+ * */
---
+++ BankingTests/StandardBonusCalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using BankingDomain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class StandardBonusCalculatorTests
+    {
+        // 	- If the deposit is made before EOB, then they get 7% added to the their deposit otherwise, they get 5%
+
+        [Fact]
+        public void DepositsMadeBeforeEOBGetSevenPercent()
+        {
+            var clock = new Mock<ISystemTime>();
+            clock.Setup(c => c.GetCurrent()).Returns(new DateTime(1969, 4, 20, 16, 59, 59));
+
+            ICalculateBonuses calculator = new StandardBonusCalculator(clock.Object);
+
+            decimal bonus = calculator.CalculateTheBonusFor(100, 7000);
+
+            Assert.Equal(7M, bonus);
+        }
+
+        [Fact]
+        public void DepositsMadeBeforeEOBGetFivePercent()
+        {
+            var clock = new Mock<ISystemTime>();
+            clock.Setup(c => c.GetCurrent()).Returns(new DateTime(1969, 4, 20, 17, 00, 00));
+            ICalculateBonuses calculator = new StandardBonusCalculator(clock.Object);
+
+            decimal bonus = calculator.CalculateTheBonusFor(100, 7000);
+
+            Assert.Equal(5M, bonus);
+        }
+
+    }
+}
